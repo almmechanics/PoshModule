5bda4223d1f0ce204b0bae91fe3d3592c54b7f87
almmechanics <martin.palmer@almmechanics.com>
2017-08-01 19:59:22 +0100

    Add files via upload

---
+++ Create-Skeleton.ps1
@@ -0,0 +1,31 @@
+﻿function Create-Skeleton
+{
+<#
+.Example
+Create-Skeleton -RootFolder c:\dev\mod1 -ModuleName 'demo'
+#>
+    [CmdletBinding()]
+    param
+    (
+        [string]
+        $RootFolder,
+        [string]
+        $ModuleName
+    )
+
+    New-Item -Path $RootFolder\$ModuleName -ItemType Directory -Force | Out-Null
+    New-Item -Path "$RootFolder\$ModuleName\cmdlets" -ItemType Directory -Force | Out-Null
+    New-Item -Path "$RootFolder\$ModuleName\cmdlets\interface" -ItemType Directory -Force | Out-Null
+    New-Item -Path "$RootFolder\$ModuleName\cmdlets\internal" -ItemType Directory -Force | Out-Null
+
+    $psd1File = "$RootFolder\$ModuleName\$ModuleName.psd1"
+
+    New-ModuleManifest -Path $psd1File -FunctionsToExport @() -RootModule "$ModuleName.psm1"
+
+    @(  "Set-StrictMode -Version Latest",
+        "`$cmdletsPath = Join-Path `$PSScriptRoot 'cmdlets'",
+        "`$interfacePath = Join-Path `$cmdletsPath 'interface'",
+        "# . (Join-Path `$interfacePath '<interface_sample>.ps1')",        
+        "`$internalPath = Join-Path `$cmdletsPath 'internal'",
+        "# . (Join-Path `$interfacePath '<internal_sample>.ps1')") | out-file "$RootFolder\$ModuleName\$ModuleName.psm1" -Encoding ascii -Force
+}
